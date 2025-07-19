09c5e3f19222fdafd99b5b11ad043dc708c22f3c
JoelTorres-25 <89654949+JoelTorres-25@users.noreply.github.com>
2025-07-19 03:39:29 -0300

    Add files via upload

--- vision/altura_agarre_Brasil.m
+++ vision/altura_agarre_Brasil.m
@@ -4,39 +4,43 @@ function [h_agarre,label] = altura_agarre_Brasil(label,z2)
     switch label
         case "Lata" 
             if z2 > -0.1060
-                h_agarre = -0.02;
+                h_agarre = 0.09;
                 label = "Lata_parada";
             else 
-                h_agarre = -0.09;
+                h_agarre = 0.0520;
                 label = "Lata_acostada";
             end
         case "Lata_parada"
-                h_agarre = -0.02;
+                h_agarre = 0.09;
                 label = "Lata_parada";
         case "Lata_acostada"
-                h_agarre = -0.09;
+                h_agarre = 0.0520;
                 label = "Lata_acostada";
         case "Botella"    
             if z2 > -0.09
-                h_agarre = 0.080;
+                h_agarre = 0.15;
                 label = "Tapa";
             else       %%%Botella acostada
-                h_agarre = -0.09;   %%valor z2 = 0.0203
+                h_agarre = 0.0520;   %%valor z2 = 0.0203
             end
         case "Tapa"
-                h_agarre = 0.080;
+                h_agarre = 0.15;
                 label = "Tapa";
         case "Cubo"
-            h_agarre = -0.097;
+            h_agarre = 0.0220;
         case "SPAM"
             if z2 > -0.11
-                h_agarre = -0.05800;
+                h_agarre = 0.0830;
             else
-                h_agarre = -0.09;
+                h_agarre = 0.0350;
                 label = "SPAM_acostado";
             end
         case "Plumon"
-            h_agarre =  -0.0950;
+            if z2>-0.05 %%parado
+                h_agarre =  0.10;
+            else 
+                h_agarre = 0.0220;
+            end
       otherwise
    end
 end
